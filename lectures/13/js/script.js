88a6d506c2e72e02705d3fec00bb74452306fcdf
raibular <rai_bular2010@ucp.edu.pk>
2018-11-30 19:04:03 +0500

    Quiz-3

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -4,7 +4,7 @@ var questions = [{
         "The Web server",
         "A central machine deep within Netscape's corporate offices",
         "None of the above"],
-    correctAnswer : 0
+    correctAnswer :0
 },{
     question : "Which of the following can't be done with client-side JavaScript?",
     choices : [ "Validating a form",
@@ -19,7 +19,25 @@ var questions = [{
         "for",
         "none of the above"],
     correctAnswer : 1
-}];
+
+
+},
+    {
+        question: "What is the meaning of Pakistan?",
+        choices: ["Muslim Land",
+            "Land of five rivers",
+            "Desert",
+            "Holy Land"],
+        correctAnswer: 3
+    },
+    {
+        question: "When did Pakistan win Olympic gold medal in Hockey for the first time?",
+        choices: ["1948",
+            "1952",
+            "1960",
+            "1964"],
+        correctAnswer: 3
+    }];
 
 var currentQuestion = 0;
 var correctAnswers = 0;
@@ -28,10 +46,58 @@ displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
     /*Write your code here */
-}
+   if(currentQuestion<4)
+   {
+       var n = document.querySelector("input[type=radio]:checked");
+       if (n == null) {
+           var displaymessage = document.getElementById("quiz-message");
+           displaymessage.style.display = "inline"
+           displaymessage.innerText = "select any option";
+           var QuestionChoice=document.getElementById("choice-list");
+           QuestionChoice.innerHTML='';
+       }
+       else{
+           if(questions[currentQuestion].correctAnswer)
+           {
+               correctAnswers++;
+           }
+           var QuestionChoice = document.getElementById("choice-list");
+           QuestionChoice.innerHTML = '';
+           currentQuestion++;
+       }
+       displayCurrentQuestion();
 
+   }
+
+   else
+   {
+       displayScore();
+
+
+   }
+
+
+
+}
+function playAgian(){
+
+    resetQuiz();
+    displayCurrentQuestion();
+}
 function displayCurrentQuestion() {
     /*Write your code here */
+    var QuizQuestion=document.getElementById("question");
+    var QuestionChoice=document.getElementById("choice-list");
+    QuizQuestion.innerHTML='<p>'+questions[currentQuestion].question + '</p>';
+    for(var i=0; i<questions[currentQuestion].choices.length;i++)
+    {
+        QuestionChoice.innerHTML+="<input type='radio' name='checked'>"+questions[currentQuestion].choices[i]+"<br>";
+    }
+
+
+
+
+
 }
 
 function resetQuiz() {
@@ -42,7 +108,18 @@ function resetQuiz() {
 function displayScore() {
     document.getElementById("result").innerHTML = "you scored: " + correctAnswers + " out of: " + questions.length;
     document.getElementById("result").style.display = 'block';
+
 }
 function hideScore() {
     document.getElementById("result").style.display = 'none';
 }
+
+
+
+
+
+
+
+
+
+
