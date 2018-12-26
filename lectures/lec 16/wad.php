72912bf56f119cf118e3c93d4d216dd9320f751a
raibular <44554296+raibular@users.noreply.github.com>
2018-12-26 17:53:13 +0500

    Add files via upload

---
+++ lectures/lec 16/wad.php
@@ -0,0 +1,17 @@
+<?php
+
+$A=Array("Trees give off oxygen that we need to breathe.","Trees provide food, protection, and homes for many birds and mammals."," Trees reduce the amount of storm water runoff");
+echo "<h1>Advantages of tree</h1>";
+for($x=0;$x<3;$x++){
+
+  echo"<li>";
+       echo $A[$x];
+   
+   echo"</li>";
+
+}
+
+
+
+
+?>
